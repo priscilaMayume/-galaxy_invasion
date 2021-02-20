18cdeb52323407b40080734c640ef5c00c360cd8
priscilaMayume <prisccila@gmail.com>
2021-02-20 10:46:22 -0300

    MVP do projeto

---
+++ Assets/scripts/enemyBlack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBlack : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private Vector2 _direction;
+
+    private Rigidbody2D _rb2dbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2dbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+
+    void Update() {
+        if (transform.position.y <= 0){
+            _direction.x = 1;
+        }
+    }
+    void FixedUpdate()
+    {
+        _rb2dbody.MovePosition(_rb2dbody.position +(_direction*_speed*Time.deltaTime));
+    }
+    void OnBecameInvisible() {
+
+        Destroy(this.gameObject);
+        
+    }
+}
---
+++ Assets/scripts/laser.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D _rb2dBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2dBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(0,speed*Time.deltaTime,0);
+    }
+
+    void FixedUpdate()
+
+    {
+        _rb2dBody.MovePosition(_rb2dBody.position + (new Vector2(0, speed * Time.deltaTime)));
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);       
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        print("Acabou de colidir");
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        print("Stay Collision");
+    }
+
+    /*void OnCollisionExit2D(Collision2D other)
+    {
+        print("Exit Collision");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Trigger Enter");
+     
+    void OnTriggerStay2D(Collider2D other)
+    {
+        print("Tigger Stay");
+    }   
+}
+*/
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       // print("Trigger Enter");
+       if (other.tag == "enemyBlack"){
+           Destroy(other.gameObject);
+           Destroy(this.gameObject);
+       }
+    }
+void OnTriggerStay2D(Collider2D other)
+    {
+        print("Tigger Stay");
+    }
+
+void OnTriggerExit2D(Collider2D other)
+    {
+        print("Tigger Exit");
+    }
+
+}
